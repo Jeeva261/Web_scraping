6f30d34e49bc68f2392381b2b718ed7ddd6bfdea
Jeeva261 <js3316573@gmail.com>
2024-09-26 14:54:26 +0530

    upload

---
+++ scarping 1.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+from sklearn.preprocessing  import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
+
+url="https://www.tutorialrepublic.com/html-tutorial/html-paragraphs.php"
+
+reesponse=requests.get(url)
+
+soup=BeautifulSoup(reesponse.content)
+
+title=soup.title.string
+
+paragraphs=[p.text
+for p in soup.find_all("p")]
+
+
+
+print(f'tittle of website:{title}')
+
+print("\nparagraphs")
+for i, paragraph in enumerate(paragraphs):
+    print(f'paragraph{i+1}:{paragraph}')
+
+
+
+data={
+    "paragraph":paragraphs,
+    "count words":[len(p.split()) for p in paragraphs]
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+
+
+encoding=LabelEncoder()
+df["paragraph_new"]=encoding.fit_transform(df["paragraph"])
+df.drop(columns="paragraph",axis=1,inplace=True)
+df.replace(0,np.nan,inplace=True)
+df.dropna(inplace=True)
+print(df)
+
+
+x=df[["paragraph_new"]]
+y=df["count words"]
+
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+
+from sklearn.preprocessing import PolynomialFeatures
+poly=PolynomialFeatures(degree=20)
+
+x_poly=poly.fit_transform(x)
+
+model=LinearRegression()
+res=model.fit(x_poly,y)
+
+y_pred_poly=model.predict(x_poly)
+
+
+
+
+
+model=LinearRegression()
+res=model.fit(x_train,y_train)
+
+
+
+print(f"slope:{model.coef_}")
+print(f"intercept:{model.intercept_}")
+
+
+y_pred=model.predict(x_test)
+print(y_pred)
+
+r2=r2_score(y_test,y_pred)
+print(f'r square:{r2}')
+
+mse=mean_squared_error(y_test,y_pred)
+print(f'mean square error:{mse}')
+
+
+plt.scatter(x_train,y_train,label="Paragraph number",color="blue")
+plt.plot(x_train,model.predict(x_train),label="count of paragraph",color="red")
+plt.title("Paragraph number vs count of paragraph")
+plt.xlabel("Paragraph number")
+plt.ylabel("count of paragraph")
+plt.legend()
+plt.show()
+
+
+plt.scatter(x,y,label="Paragraph number",color="blue")
+plt.plot(x,y_pred_poly,label="count of paragraph",color="red")
+plt.title("Paragraph number vs count of paragraph")
+plt.xlabel("Paragraph number")
+plt.ylabel("count of paragraph")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+new_selection=4.0
+new_selection_df=model.predict([[new_selection]])
+print(new_selection_df)
+
+
--- scarping.py
+++ scarping.py
@@ -1,1 +1,64 @@
+import requests
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import pandas as pd
 
+url="https://weather.com/en-IN/weather/tenday/l/Salem+Tamil+Nadu?canonicalCityId=6bac361845d6b7a37c1b5cdc646fe6d09fc9e19fbbb034945b1307421f76106d"
+
+response=requests.get(url)
+
+soup=BeautifulSoup(response.content)
+
+title=soup.find("title").text
+print(title)
+
+paragraphs=soup.find("p").text
+print(paragraphs)
+
+paragraphs=[p.text for p in soup.find_all("p")]
+print(paragraphs,"\n\n\n")
+for i,paragraph in enumerate(paragraphs):
+    print(f'paragraph{i+1}:{paragraph}')
+
+
+spans=soup.find_all("span")
+for span in spans:
+    print(span.prettify(),"\n")
+
+
+
+date=[date.text for date in soup.find_all("h2", class_="DailyContent--daypartName--3emSU")]
+print(date,"\n\n")
+
+
+
+spans=[span.text for span in soup.find_all("span")]  #class_="DailyContent--degreeSymbol--EbEpi")]
+
+for i,snap in enumerate(spans):
+    print(f"span{i+1}:{snap}")
+
+
+
+span=[span.text for span in soup.find_all("span", dir="ltr")]
+print(span,"\n")
+
+
+# web scraping and taking with dataframe
+
+date_array=pd.DataFrame(date)
+print(date_array)
+span_array=pd.DataFrame(span)
+print(span_array)
+
+
+data={
+    "date":date_array[0],
+    # "day":[date],
+    "degree":span_array[0]
+         
+}
+
+df=pd.DataFrame(data)
+df.dropna(inplace=True)
+print(df)
